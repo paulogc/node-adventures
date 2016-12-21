ec1876ca9616b21c38bf8ab22881bea472d1f9ed
Paulo <pauloguedescamargo@gmail.com>
2016-12-20 22:13:45 -0200

    Class 11

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
-let user = os.userInfo();
+const _ = require('lodash');
 
-fs.appendFile('new.txt', `Menelique É um gato louco `);
+console.log(_.isString(true));
+console.log(_.isString('true'));
 
-fs.appendFile('new.txt', `Hello ${user.username} `);
+var filterArray = _.uniq([1, 2, 3, 2 ,1, 4, 4, 4, 'Paulo', 'Paulo']);
+console.log(filterArray)
+
+// console.log('Result', notes.sum(4, 5));
+
+// let user = os.userInfo();
+
+// fs.appendFile('new.txt', `Hello ${user.username} You are ${notes.age}.`);
---
+++ notes.js
@@ -0,0 +1,10 @@
+console.log('statrting notes');
+
+module.exports.addNote = () => {
+  console.log('add note');
+  return('new notes');
+};
+
+module.exports.sum = (a, b) => {
+  return a + b;
+};
