f59e11141a8fc0ae729082a103882f68757a0c24
Paulo <pauloguedescamargo@gmail.com>
2017-01-03 22:12:41 -0200

    weather start

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,10 +14,21 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
+var results;
+const key = '4c19496c089f8130403a97c387246119';
 geocode.geocodeAddress(argv.a, (errorMessage, results) => {
   if (errorMessage) {
     console.log(errorMessage);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    geocode.weatherAddress(results, key, (weatherErrorMessage, weatherResults) => {
+      if (weatherErrorMessage) {
+        consoel.log(weatherErrorMessage);
+      } else {
+        console.log(JSON.stringify(results, undefined, 2));
+        console.log(JSON.stringify(weatherResults, undefined, 2));
+      }
+    })
   }
+
+
 });
