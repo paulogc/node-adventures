94036c95e96a08a65b14865e5d7eb1f047797acd
Paulo <pauloguedescamargo@gmail.com>
2017-01-15 19:22:19 -0200

    new features

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-//app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintence.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: 'About page',
-    currentYear: new Date().getFullYear(),
+    pageTitle: 'Some page',
     welcomeMessage: 'Welcome Friend',
   });
 });
@@ -18,7 +43,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
