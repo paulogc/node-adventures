44e8b6680c232c66a673f655e89817c3d27c60a4
Paulo <pauloguedescamargo@gmail.com>
2017-01-18 21:07:10 -0200

    simple mods

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ app.get('/about/me', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is up and running on port: ${port}`);
+  console.log(`Server is up and running on port yes: ${port}`);
 });
