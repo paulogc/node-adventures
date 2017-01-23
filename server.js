3b5d39a7216d23c625f165ad212f8b7e84a9e513
Paulo <pauloguedescamargo@gmail.com>
2017-01-23 20:23:30 -0200

    New page projects

--- server.js
+++ server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     error: 35,
